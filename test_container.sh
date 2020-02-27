aec82412c63c8d01784b98d51aa6df36a1ad764e
tigelane <tige@ignw.io>
2020-02-27 12:08:02 -0800

    variables for the script

--- test_container.sh
+++ test_container.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-if docker inspect -f '{{.State.Running}}' devnet_create_2020
+if docker inspect -f '{{.State.Running}}' $1
 then
   echo Stopping the container
-  docker stop devnet_create_2020
+  docker stop $1
 else
   echo No need to stop the container
 fi
